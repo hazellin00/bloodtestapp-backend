6b34a09e40445baa77293744dfeb89d44c65f9d0
hazellin00 <hazellin63@gmail.com>
2026-03-03 23:17:56 +0800

    Initial backend deployment

--- src/components/MedicalDisclaimer.tsx
+++
@@ -1,9 +0,0 @@
-import { MEDICAL_DISCLAIMER } from "@/lib/bp-utils";
-
-const MedicalDisclaimer = () => (
-  <p className="medical-disclaimer text-center">
-    ⚕️ {MEDICAL_DISCLAIMER}
-  </p>
-);
-
-export default MedicalDisclaimer;
